766dd9d897330123793621444c6172a090723291
quoeamaster <quoeamaster@gmail.com>
2018-06-01 12:13:25 +0800

    2018-06-01a

---
+++ Dummy.go
@@ -0,0 +1,10 @@
+/**
+ *  no logic inside this file; just added for the purpose of letting 'dep ensure' to work
+ */
+package CFactor
+
+import "fmt"
+
+func Testing() {
+	fmt.Println("added this file for 'dep ensure' to work only... no logic inside")
+}
